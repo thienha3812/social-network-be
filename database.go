9c8d6f4ca9ee7e3418bf11d2525a7db5fb2010ef
Your Name <you@example.com>
2020-06-16 23:41:43 +0700

    123

--- database.go
+++ database.go
@@ -10,8 +10,8 @@ type Database struct {
 }
 
 func (*Database) init() *gorm.DB {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=test sslmode=disable password=postgres")
-	// db, err := gorm.Open("postgres", "host=ec2-34-197-141-7.compute-1.amazonaws.com port=5432 user=eeogohfzqibxdd dbname=der1geoiabi8nk password=20da858827b726ecbff23779708cd57f281805ed11722dfd03aaa2d779004cca")
+	// db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=test sslmode=disable password=postgres")
+	db, err := gorm.Open("postgres", "host=ec2-34-197-141-7.compute-1.amazonaws.com port=5432 user=eeogohfzqibxdd dbname=der1geoiabi8nk password=20da858827b726ecbff23779708cd57f281805ed11722dfd03aaa2d779004cca")
 	if err != nil {
 		panic(err)
 	}
