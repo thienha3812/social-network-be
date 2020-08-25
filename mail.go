31f62ec4e6862e8e6b5365db25b37bf1119422e0
thienha3812 <thienhamaimai1@gmail.com>
2020-08-25 21:51:49 +0700

    Update mail.go

--- mail.go
+++ mail.go
@@ -10,7 +10,7 @@ type Mail struct {
 	Msg []byte
 }
 func sendMail( email <-chan Mail){
-	auth := smtp.PlainAuth("", "thienhamaimai1@gmail.com", "fupkijjadpvatryi", "smtp.gmail.com")
+	auth := smtp.PlainAuth("", "thienhamaimai1@gmail.com", "", "smtp.gmail.com")
 	for e := range email {
 		if err := smtp.SendMail("smtp.gmail.com:587", auth, "thienhamaimai1@gmail.com", e.To, e.Msg); err != nil {
 			log.Fatal(err)
